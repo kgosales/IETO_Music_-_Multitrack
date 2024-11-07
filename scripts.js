6953eec0d62d098ac2c900e3dd6258201d243762
Gabriel Sales <kleythongabriel@outlook.com>
2024-11-06 22:30:41 -0300

    Ordenação Alfabética

--- scripts.js
+++ scripts.js
@@ -3,11 +3,16 @@ const lista = document.querySelector("#lista");
 fetch("data/multitracks.json")
     .then(response => response.json())
     .then(multitracks => {
-        for (const multitrack of multitracks) {
+
+        multitracks.sort((a, b) => a.artista.localeCompare(b.artista) || a.musica.localeCompare(b.musica));
+
+        const fragment = document.createDocumentFragment();
+
+        multitracks.forEach((multitrack, index) => {
             const div = document.createElement("div");
             div.className = "multitrack";
             div.innerHTML = `
-                <p class="numero">${multitracks.indexOf(multitrack) + 1}</p>
+                <p class="numero">${index + 1}</p>
                 <div class="info">
                     <p><span>Artista: </span>${multitrack.artista}</p>
                     <p><span>Música: </span>${multitrack.musica}</p>
@@ -15,21 +20,13 @@ fetch("data/multitracks.json")
                 </div>
                 <div class="links">
                     <span>Links</span>
-                    <a href=${multitrack.link_youtube || "#"} target="_blank"><img src=icons/youtube.png ></a>
-                    <a href=${multitrack.link_download || "#"} target="_blank"><img src=icons/download.png ></a>
+                    <a href=${multitrack.link_youtube || "#"} target="_blank" ${!multitrack.link_youtube ? 'disabled' : ''}><img src=icons/youtube.png ></a>
+                    <a href=${multitrack.link_download || "#"} target="_blank" ${!multitrack.link_download ? 'disabled' : ''}><img src=icons/download.png ></a>
                 </div>
-                
             `;
-            lista.appendChild(div);
-        }
-
-        const links = document.querySelectorAll(".links a");
-
-        links.forEach((link) => {
-            if (link.href.includes("#")) {
-                link.setAttribute("disabled", "true");
-            }
+            fragment.appendChild(div);
         });
-    })
 
-    .catch(error => console.error(error))
+        lista.appendChild(fragment);
+    })
+    .catch(error => console.error(error));
